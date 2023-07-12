6cef24752121f725624a404aa75e498ada99e995
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-07-12 17:31:30 +0900

    feat: [유경우] 주식 데이터를 요청하고 보내줍니다[SPP-2]

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from save_ticker.save_ticker_router import save_ticker_router
 from stock_test.stock_test_router import stock_test_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8080",  # 허용하려는 프론트엔드 도메인 주소
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -12,6 +24,3 @@ def read_root():
 app.include_router(stock_test_router)
 app.include_router(save_ticker_router)
 
-# @app.get("/stock/{ticker}")
-# async def stock_ticker(ticker:str):
-#     return {'ticker' : ticker}
--- stock_test/stock_test_router.py
+++ stock_test/stock_test_router.py
@@ -2,13 +2,33 @@ from fastapi import APIRouter
 import yfinance as yf
 
 stock_test_router = APIRouter()
-@stock_test_router.get("/stock/test")
-async def stock_OHCL_test():
 
-    result = yf.download('005930.KS', interval='1m', period='1d')
-    minute_data = result[['Open', 'High', 'Close', 'Low']]
-    json_data = minute_data.to_json(orient='records')
+# @app.get("/stock/{ticker}")
+# async def stock_ticker(ticker:str):
+#     return {'ticker' : ticker}
 
-    return json_data
+@stock_test_router.get("/stock/{ticker}/{period}/{interval}")
+async def stock_OHCL_test(ticker:str, period:str, interval:str):
+
+    ticker = ticker + '.KS'
+
+    result = yf.download(ticker, interval=interval, period=period)
+    minute_data = []
+    for index, row in result.iterrows():
+        date = index.strftime('%Y-%m-%d %H:%M')  # 날짜, 시간, 분을 문자열로 변환
+        open_value = row['Open']
+        high_value = row['High']
+        low_value = row['Low']
+        close_value = row['Close']
+
+        minute_data.append({
+            'Date': date,
+            'Open': open_value,
+            'High': high_value,
+            'Low': low_value,
+            'Close': close_value
+        })
+
+    return (minute_data)
 
 
