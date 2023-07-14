1b9e8f31423e2275f636baa3f5b6d2b234588f20
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-07-14 11:50:48 +0900

    feat: [유경우] 요청받은 종목의 기사를 크롤링합니다[SPP - 3]

--- news_crawler/crawler_test.py
+++ news_crawler/crawler_test.py
@@ -1,19 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
-from fastapi import APIRouter
+from fastapi import FastAPI, APIRouter
+from urllib.parse import unquote
+
+app = FastAPI()
 
 news_crawler_router = APIRouter()
+
 @news_crawler_router.get("/article/{stock_name}")
-def news_crawler(stock_name: str):
-    url = f"https://search.naver.com/search.naver?where=news&sm=tab_jum&query={stock_name}"
+async def news_crawler(stock_name: str):
+    decoded_stock_name = unquote(stock_name)
+
+    url = f"https://search.naver.com/search.naver?where=news&sm=tab_jum&query={decoded_stock_name}"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     news_titles = soup.select("a.news_tit")
     news_companies = soup.select("a.info.press")
-    news_images = soup.select("img.thumb.api_get")
 
-    titles = [title['title'] for title in news_titles]
-    hrefs = [title['href'] for title in news_titles]
-    companies = [company.text for company in news_companies]
-    images = [img['src'] for img in news_images]
+    # 배열 형식으로 구성
+    data = [
+      {
+        "title": title['title'],
+        "href": title['href'],
+        "company": company.text
+      }
+      for title, company in zip(news_titles, news_companies)
+    ]
+
+    return data
+
+app.include_router(news_crawler_router)
