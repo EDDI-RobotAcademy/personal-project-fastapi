cccafcd08dc1a4b4069e4bb94d2c6f9d4aa6cb78
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-07-21 15:15:44 +0900

    feat: [유경우] 크롤러에 멀티스레드 더하기[SPP-9]

--- opinion_mining/board_crawler/board_crawler.py
+++ opinion_mining/board_crawler/board_crawler.py
@@ -1,47 +1,65 @@
+import concurrent.futures
 import re
+import time
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Pool
+
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 class BoardCrawler():
-    def board_crawler(self, ticker:str):
+    def __init__(self, ticker):
+
+        self.ticker = ticker
+        self.url_list = []
+
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("headless")
-        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
-        driver = webdriver.Chrome(executable_path='D:/project/personal-project-fastapi/opinion_mining/board_crawler/chromedriver.exe', chrome_options=chrome_options)
+        chrome_options.add_argument(
+            "user-agent=Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36")
+        self.driver = webdriver.Chrome(
+            executable_path='D:/project/personal-project-fastapi/opinion_mining/board_crawler/chromedriver.exe',
+            options=chrome_options)
 
         code = ticker
+        self.crawling_result = []
 
-        crawling_result = []
+        self.wait = WebDriverWait(self.driver, 1)
 
-        for i in range(1, 2):
+        for i in range(1, 11):
             URL = f"https://finance.naver.com/item/board.naver?code={code}&page={i}"
+            self.url_list.append(URL)
+    def multi_thread_crawl(self):
+        with ThreadPoolExecutor(max_workers=12) as executor:
+            result = list(executor.map(self.board_crawler, self.url_list))
 
-            driver.get(URL)
-            wait = WebDriverWait(driver, 1)
+        self.driver.quit()
+        return result
+    def board_crawler(self, url):
+        self.driver.get(url)
 
-            titles = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "title")))
+        titles = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "title")))
 
-            link_list = []
+        link_list = []
 
-            for title in titles:
-                a_tag = title.find_element(By.TAG_NAME, "a")
-                link = a_tag.get_attribute("href")
-                link_list.append(link)
+        for title in titles:
+            a_tag = title.find_element(By.TAG_NAME, "a")
+            link = a_tag.get_attribute("href")
+            link_list.append(link)
 
-                title_text = title.text
-                title_text = re.sub(r'[-=+,#/\?:^.\@*\"※~ㆍ!』‘\|\(\)\[\]`\'…》\”\“\’·a-zA-Z0-9\n]', '', title_text)
-                crawling_result.append(title_text)
+            title_text = title.text
+            title_text = re.sub(r'[-=+,#/\?:^.\@*\"※~ㆍ!』‘\|\(\)\[\]`\'…》\”\“\’·a-zA-Z0-9\n]', '', title_text)
+            self.crawling_result.append(title_text)
 
-            for link in link_list:
-                original_url = driver.current_url
-                driver.get(link)
+        for link in link_list:
+            self.driver.get(link)
 
-                content = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "scr01"))).text
-                content = re.sub(r'[-=+,#/\?:^.\@*\"※~ㆍ!』‘\|\(\)\[\]`\'…》\”\“\’·a-zA-Z0-9\n]', '', content)
-                crawling_result.append(content)
-                driver.get(original_url)
+            content = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, "scr01"))).text
+            content = re.sub(r'[-=+,#/\?:^.\@*\"※~ㆍ!』‘\|\(\)\[\]`\'…》\”\“\’·a-zA-Z0-9\n]', '', content)
+            self.crawling_result.append(content)
+            # self.driver.get(url)
 
-        driver.quit()
-        return crawling_result
+        return self.crawling_result
+
--- opinion_mining/opinion_mining_router.py
+++ opinion_mining/opinion_mining_router.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from fastapi import FastAPI, APIRouter
 
@@ -11,7 +12,7 @@ opinion_mining_router = APIRouter()
 with open("SentiWord_info.json", "r", encoding='utf-8') as f:
     sentiment_dictionary = json.load(f)
 
-class SentiScoreResult:
+class OpinionMiningResult:
     def __init__(self):
         self.total_sentiment_score = 0
         self.total_positive_count = 0
@@ -26,19 +27,33 @@ class SentiScoreResult:
 
 @opinion_mining_router.get("/opinion-mining/{ticker}")
 async def opinion_mining(ticker: str):
-    result = SentiScoreResult()
+    start_time = time.time()  # 시작 시간 저장
 
-    crawling_result = BoardCrawler().board_crawler(ticker)
+    senti_score_result = OpinionMiningResult()
+
+    crawler = BoardCrawler(ticker)
+    crawling_result = crawler.multi_thread_crawl()
+    print("crawling_result: ", crawling_result)
+
+    end_time = time.time()
+    total_time = end_time - start_time  # 총 작동시간 계산
+    total_time_int = int(total_time)  # 소수점 이하 자리수 탈락
 
     for item in crawling_result:
         tokens = KiwiTokenizer().kiwi_tokenize(item)
+        print("tokens: ", tokens)
 
         sentiment_score = SentiWordCalculator(tokens, sentiment_dictionary)
-        result.update(sentiment_score)
+        print("sentiment_score: ", sentiment_score)
+        senti_score_result.update(sentiment_score)
+        print("SentiScoreResult(): ", OpinionMiningResult())
+
+
 
     return {
-        "total_sentiment_score": result.total_sentiment_score,
-        "total_positive_count": result.total_positive_count,
-        "total_negative_count": result.total_negative_count,
-        "total_neutral_count": result.total_neutral_count,
+        "total_sentiment_score": senti_score_result.total_sentiment_score,
+        "total_positive_count": senti_score_result.total_positive_count,
+        "total_negative_count": senti_score_result.total_negative_count,
+        "total_neutral_count": senti_score_result.total_neutral_count,
+        "total_time": total_time_int,  # 총 작동시간 추가
     }
