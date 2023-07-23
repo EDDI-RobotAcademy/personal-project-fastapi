81c5642c7614e6313e552dc6d8c688358d7007ec
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-07-23 14:21:25 +0900

    feat: [유경우] 기사를 페이지 별로 볼 수 있습니다[SPP-13]

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from news_crawler.crawler_test import news_crawler_router
+from article.news_crawler.crawler_test import news_crawler_router
 from opinion_mining.opinion_mining_router import opinion_mining_router
 from stock.save_ticker.save_ticker_router import save_ticker_router
 from stock.stock_OHCL_response.stock_router import stock_response_router
+from stock.stock_top30_reponse.top_stock_router import top30_router
 
 app = FastAPI()
 
@@ -25,7 +26,7 @@ app.add_middleware(
 def read_root():
     return {"Hello": "World"}
 
-
+app.include_router(top30_router)
 app.include_router(opinion_mining_router)
 app.include_router(stock_response_router)
 app.include_router(save_ticker_router)
